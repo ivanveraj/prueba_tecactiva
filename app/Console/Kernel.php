7433cae1ee4e69b55f9eab8dcde4a194f2d2b2ed
Jaider Ivan Vera Jaimes <jsjlds66@gmail.com>
2022-03-03 14:40:07 -0500

    cambio

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('test:work')->everyMinute();
+        $schedule->command('test:work')->everyTwoMinutes();
     }
 
     /**
